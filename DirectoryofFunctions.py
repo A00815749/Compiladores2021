85a52b19294df72846ac745171b0a848f4365dee
A00815749 <56553263+A00815749@users.noreply.github.com>
2021-11-07 22:04:34 -0600

    Datastructures added
    Adding the datastructures for semantic operator checking, variable and function storage

---
+++ DirectoryofFunctions.py
@@ -0,0 +1,50 @@
+from Vartables import Vartables
+import sys
+
+#The Directory for functions and all that fun 
+class DirectoryFunctions:
+    def _init_(self):
+        self.directory = {}
+
+    def addfunc(self, type, functionID,numPar,typePar,nameofPar, numVars):
+        if(functionID not in self.directory.keys()): #Is the function already in the list to not cause conflicts
+            self.directory[functionID] = {
+                'type' : type,
+                'numPar' : numPar,
+                'typePar' : typePar,
+                'nameofPar' : nameofPar,
+                'localvars' : Vartables(),
+                #probably using a method later for the numvars
+                'numberVars' : numVars 
+            }
+            print("Check, function ", functionID, ' ', type)
+
+        else:
+            print ("Error, function already in the directory of functions ", functionID)
+
+    def searchFunc(self,id): #Search method
+        return id in self.directory
+
+    def addVar(self,functionID, type, currentID):
+        if (self.directory[functionID]['localvars'].searchVariable(currentID)):
+            print("This variable is already in the function ", currentID)
+        else:
+            self.directory[functionID]['localvars'].add(currentID, type)
+            print("Var added to function local table ", functionID)
+
+    def printfuncvariables(self,functionID):
+        if functionID in self.directory:
+            self.directory[functionID]['localvars'].printVariable()
+
+
+    def printfunc(self,functionID):
+        print("ID " + functionID)
+        print("Type " + self.directory[functionID]['type'])
+        print("Number of Parameters " + self.directory[functionID]['numPar'])
+        print("type of Parameters : ")
+        print(self.directory[functionID]['typePar'])
+        print("Name of Parameters : ")
+        print(self.directory[functionID]['nameofPar'])
+        print("Name Of Variables : ")
+        self.printfuncvariables(functionID)
+        print("Number Of Variables: " + self.directory[functionID]['numberVars'])
